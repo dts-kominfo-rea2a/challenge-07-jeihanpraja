59f32d1396d95d43f90973b6b6a317049474cffc
Jeihan Praja Yudanto <jeihan.yudanto@gmail.com>
2022-06-12 21:17:17 +0700

    challenge-07-jeihanpraja-versi-1

--- promise.js
+++ promise.js
@@ -1,8 +1,28 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
 
-module.exports = {
-  promiseOutput,
+  const emosiIXX = await promiseTheaterIXX();
+  const emosiVGC = await promiseTheaterVGC();
+
+  return new Promise((resolve) => {
+    let hasil;
+
+    if(emosi === 'marah') {
+      const emosiIXXMarah = emosiIXX.filter((element) => (element.hasil === 'marah')).length;
+      const emosiVGCMarah = emosiVGC.filter((element) => (element.hasil === 'marah')).length;
+      hasil = emosiIXXMarah + emosiVGCMarah;
+    } else if (emosi === 'tidak marah') {
+      const emosiIIXTdkMarah = emosiIXX.filter((element) => (element.hasil === 'tidak marah')).length;
+      const emosiVGCTdkMarah = emosiVGC.filter((element) => (element.hasil === 'tidak marah')).length;
+      hasil = emosiIIXTdkMarah + emosiVGCTdkMarah;
+    }
+
+    return resolve(hasil);
+  })
 };
+
+  module.exports = {
+      promiseOutput,
+    };
